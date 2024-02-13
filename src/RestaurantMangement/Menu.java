a0477d7e95267cf91c94aa71ed675b1e16135777
ALPHAROZER <158816457+ALPHAROZER@users.noreply.github.com>
2024-02-13 11:22:07 +0530

    Update Menu.java

--- src/RestaurantMangement/Menu.java
+++ src/RestaurantMangement/Menu.java
@@ -25,6 +25,13 @@ public class Menu extends javax.swing.JFrame {
         setImages();
     }
     
+    public void gotologin() {
+        Admin admin = new Admin();
+        admin.setVisible(true);
+        admin.pack();
+        this.dispose();
+    }
+    
         public void gotodash() {
         DashBoard dashBoard = new DashBoard();
         dashBoard.setVisible(true);
@@ -32,28 +39,27 @@ public class Menu extends javax.swing.JFrame {
         this.dispose();
     }
     public void setImages() {
-        ImageIcon icon1 = new ImageIcon(getClass().getResource("/images/DatabaseImg.png"));
+        ImageIcon icon1 = new ImageIcon(getClass().getResource("/images/LoginImg.png"));
         ImageIcon icon2 = new ImageIcon(getClass().getResource("/images/dashboardImg.png"));
         
-        Image img1 = icon1.getImage().getScaledInstance(imgdb.getWidth(), imgdb.getHeight(), Image.SCALE_SMOOTH);
+        Image img1 = icon1.getImage().getScaledInstance(imgLogin.getWidth(), imgLogin.getHeight(), Image.SCALE_SMOOTH);
         Image img2 = icon2.getImage().getScaledInstance(imgdash.getWidth(), imgdash.getHeight(), Image.SCALE_SMOOTH);
         
-        imgdb.setIcon(new ImageIcon(img1));
+        imgLogin.setIcon(new ImageIcon(img1));
         imgdash.setIcon(new ImageIcon(img2));
     }
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">                          
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
         dbOption = new javax.swing.JPanel();
-        imgdb = new javax.swing.JLabel();
+        imgLogin = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         dashOption = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         imgdash = new javax.swing.JLabel();
         jButtonExit = new javax.swing.JButton();
-        jButtonLogin = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -61,22 +67,27 @@ public class Menu extends javax.swing.JFrame {
 
         dbOption.setBackground(new java.awt.Color(255, 255, 255));
         dbOption.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 3, true));
+        dbOption.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                dbOptionMouseClicked(evt);
+            }
+        });
 
         jLabel1.setFont(new java.awt.Font("Comic Sans MS", 1, 24)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Database");
+        jLabel1.setText("Login");
 
         javax.swing.GroupLayout dbOptionLayout = new javax.swing.GroupLayout(dbOption);
         dbOption.setLayout(dbOptionLayout);
         dbOptionLayout.setHorizontalGroup(
             dbOptionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(imgdb, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(imgLogin, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 204, Short.MAX_VALUE)
         );
         dbOptionLayout.setVerticalGroup(
             dbOptionLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(dbOptionLayout.createSequentialGroup()
-                .addComponent(imgdb, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(imgLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -121,33 +132,21 @@ public class Menu extends javax.swing.JFrame {
             }
         });
 
-        jButtonLogin.setBackground(new java.awt.Color(0, 204, 255));
-        jButtonLogin.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
-        jButtonLogin.setForeground(new java.awt.Color(255, 255, 255));
-        jButtonLogin.setText("Login");
-        jButtonLogin.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 204, 255), 2, true));
-        jButtonLogin.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonLoginActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(147, 147, 147)
-                .addComponent(dbOption, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(137, 137, 137)
-                .addComponent(dashOption, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(147, 147, 147)
+                        .addComponent(dbOption, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(137, 137, 137)
+                        .addComponent(dashOption, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(371, 371, 371)
+                        .addComponent(jButtonExit, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(158, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButtonLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(30, 30, 30)
-                .addComponent(jButtonExit, javax.swing.GroupLayout.PREFERRED_SIZE, 119, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(291, 291, 291))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -156,11 +155,9 @@ public class Menu extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(dbOption, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(dashOption, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(29, 29, 29)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButtonLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButtonExit, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(22, 22, 22))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+                .addComponent(jButtonExit, javax.swing.GroupLayout.PREFERRED_SIZE, 43, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(24, 24, 24))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -175,22 +172,19 @@ public class Menu extends javax.swing.JFrame {
         );
 
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }// </editor-fold>                        
 
-    private void jButtonLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonLoginActionPerformed
-        Admin admin = new Admin();
-                admin.setVisible(true);
-                admin.pack();
-                this.dispose();
-    }//GEN-LAST:event_jButtonLoginActionPerformed
-
-    private void jButtonExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonExitActionPerformed
+    private void jButtonExitActionPerformed(java.awt.event.ActionEvent evt) {                                            
         System.exit(0);
-    }//GEN-LAST:event_jButtonExitActionPerformed
+    }                                           
 
-    private void dashOptionMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_dashOptionMouseClicked
+    private void dashOptionMouseClicked(java.awt.event.MouseEvent evt) {                                        
         gotodash();
-    }//GEN-LAST:event_dashOptionMouseClicked
+    }                                       
+
+    private void dbOptionMouseClicked(java.awt.event.MouseEvent evt) {                                      
+        gotologin();
+    }                                     
 
     /**
      * @param args the command line arguments
@@ -227,15 +221,14 @@ public class Menu extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
+    // Variables declaration - do not modify                     
     private javax.swing.JPanel dashOption;
     private javax.swing.JPanel dbOption;
+    private javax.swing.JLabel imgLogin;
     private javax.swing.JLabel imgdash;
-    private javax.swing.JLabel imgdb;
     private javax.swing.JButton jButtonExit;
-    private javax.swing.JButton jButtonLogin;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
-    // End of variables declaration//GEN-END:variables
+    // End of variables declaration                   
 }
